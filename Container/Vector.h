17e35fac72be69712236d743223b86619b489e4a
haolly <andyhaolly@gmail.com>
2017-12-03 23:53:09 +0800

    continue add fundamental infrastructure

--- Container/Vector.h
+++ Container/Vector.h
@@ -7,6 +7,7 @@
 
 #include <cstring>
 #include <cassert>
+#include <initializer_list>
 #include "VectorBase.h"
 
 namespace Urho3D
@@ -58,13 +59,163 @@ namespace Urho3D
 		{
 			*this = vector;
 		}
-		//todo
+
+		//Todo, : PODVector() is what ?
+		PODVector(const std::initializer_list<T>& list) : PODVector()
+		{
+			for (auto it = list.begin(); it != list.end(); ++it)
+			{
+				Push(*it);
+			}
+		}
+
+		~PODVector()
+		{
+			delete [] buffer_;
+		}
+
+		PODVector<T>&operator =(const PODVector<T>& rhs)
+		{
+			if(&rhs != this)
+			{
+				Resize(rhs.size_);
+				CopyElements(buffer_, rhs.buffer_, rhs.size_);
+			}
+			return *this;
+		}
+
+		PODVector<T>&operator +=(const T& rhs)
+		{
+			Push(rhs);
+			return *this;
+		}
+
+		PODVector<T> operator +(const T& rhs) const
+		{
+			PODVector<T> ret(*this);
+			ret.Push(rhs);
+			return ret;
+		}
+
+		PODVector<T> operator+(const PODVector<T>& rhs) const
+		{
+			PODVector<T> ret(*this);
+			ret.Push(rhs);
+			return ret;
+		}
+		
+		bool operator ==(const PODVector<T>& rhs) const 
+		{
+			if(rhs.size_ != size_)
+				return false;
+			T* buffer = Buffer();
+			T* rhsBuffer = rhs.Buffer();
+			for (int i = 0; i < size_; ++i) {
+				if(buffer[i] != rhsBuffer[i])
+					return false;
+			}
+			return true;
+		}
+
+		bool operator !=(const PODVector<T>& rhs) const
+		{
+			if(rhs.size_ != size_)
+				return true;
+
+			T* buffer = Buffer();
+			T* rhsBuffer = rhs.Buffer();
+			for (int i = 0; i < size_; ++i) {
+				if(buffer[i] != rhsBuffer[i])
+					return true;
+			}
+			return false;
+		}
+
+		T&operator [](unsigned index)
+		{
+			assert(index < size_);
+			return Buffer()[index];
+		}
+
+		const T&operator [](unsigned index) const
+		{
+			assert(index < size_);
+			return Buffer()[index];
+		}
 
 		T& At(unsigned index)
 		{
 			assert(index < size_);
 			return Buffer()[index];
 		}
+
+		const T& At(unsigned index) const
+		{
+			assert(index < size_);
+			return Buffer()[index];
+		}
+
+		void Push(const T& value)
+		{
+			if(size_ < capacity_)
+				size_++;
+			else
+				Resize(size_ + 1);
+			Back() = value;
+		}
+
+		void Push(const PODVector<T>& rhs)
+		{
+			unsigned oldSize = size_;
+			Resize(size_ + rhs.size_);
+			CopyElements(buffer_ + oldSize, rhs.buffer_, rhs.size_);
+		}
+
+		void Pop()
+		{
+			if(size_)
+				Resize(size_ - 1);
+		}
+
+		void Insert(unsigned pos, const T& value)
+		{
+			if(pos > size_)
+				pos = size_;
+			unsigned oldSize = size_;
+			Resize(size_ + 1);
+			MoveRange(pos + 1, pos, oldSize - pos);
+			Buffer()[pos] = value;
+		}
+
+		void Insert(unsigned pos, const PODVector<T>& vector)
+		{
+			if(pos > size_)
+				pos = size_;
+			unsigned oldSize = size_;
+			Resize(size_ + vector.size_);
+			MoveRange(pos + vector.size_, pos, oldSize - pos);
+			CopyElements(buffer_ + pos, vector.buffer_, vector.size_);
+		}
+
+		Iterator Insert(const Iterator& dest, const T& value)
+		{
+			unsigned pos =(unsigned)(Begin() - dest);
+			if(pos > size_)
+				pos = size_;
+			Insert(pos, value);
+			return Begin() + pos;
+		}
+
+		Iterator Insert(const Iterator& dest, const ConstIterator& start, const ConstIterator& end)
+		{
+			unsigned pos = (unsigned) (dest - Begin());
+			if(pos > size_)
+				pos = size_;
+			unsigned length = (unsigned)(end - start);
+			Resize(size_ + length);
+			//todo
+		}
+
 
 		void Clear()
 		{
@@ -95,6 +246,59 @@ namespace Urho3D
 			}
 			size_ = newSize;
 		}
+
+		void Reserve(unsigned newCapacity)
+		{
+			if(newCapacity < size_)
+				newCapacity = size_;
+
+			if(newCapacity != capacity_)
+			{
+				byte* newBuffer = nullptr;
+				capacity_ = newCapacity;
+				if(capacity_)
+				{
+					newBuffer = AllocateBuffer((unsigned)capacity_ * sizeof(T));
+					CopyElements(newBuffer, buffer_, size_);
+				}
+			}
+		}
+
+
+		Iterator Begin() { return Iterator(Buffer()); }
+
+		ConstIterator Begin() const
+		{
+			return ConstIterator(Buffer());
+		}
+
+		Iterator End() { return Iterator(Buffer() + size_); }
+
+		ConstIterator End() const
+		{
+			return ConstIterator(Buffer() + size_);
+		}
+
+		T& Front() { return Buffer()[0]; }
+		const T& Front() const { return Buffer()[0]; }
+
+		T& Back()
+		{
+			assert(size_);
+			return Buffer()[size_ -1];
+		}
+
+		const T& Back() const
+		{
+			assert(size_);
+			return Buffer()[size_ - 1];
+		}
+
+		unsigned Size() const { return size_; }
+
+		unsigned Capacity() const { return capacity_; }
+
+		bool Empty() const { return size_ == 0; }
 
 		T* Buffer() const
 		{
